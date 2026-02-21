d2de8faa276013662cfd906a708ca3363191c5e3
KyleFuller <56740521+KyleFuller@users.noreply.github.com>
2026-02-20 23:35:13 -0500

    added a couple helpful log2 grind lemmas relating to monotonicity.

--- IUAI2/c21/LogLemmas.lean
+++ IUAI2/c21/LogLemmas.lean
@@ -19,7 +19,7 @@ lemma log2_mono {n m : ℕ} : n ≤ m → n.log2 ≤ m.log2 := by
 lemma log2_two_mul (n : ℕ) (h : n ≠ 0) : (2 * n).log2 = n.log2 + 1 := Nat.log2_two_mul h
 
 @[grind ., grind! ., grind! →]
-lemma log2_of_plus_le_log2 {n m : ℕ} : m ≤ n → (n + m).log2 ≤ n.log2 + 1 := by
+lemma log2_add_le_log2 {n m : ℕ} : m ≤ n → (n + m).log2 ≤ n.log2 + 1 := by
   intro h
   have : m ≤ n := h
 
@@ -29,3 +29,11 @@ lemma log2_of_plus_le_log2 {n m : ℕ} : m ≤ n → (n + m).log2 ≤ n.log2 + 1
   | n' + 1 =>
     have : (n + m).log2 ≤ (2 * n).log2 := log2_mono (by grind)
     grind only [log2_two_mul]
+
+@[grind ., grind! .]
+lemma log2_le_log2_add (n m : ℕ) : n.log2 ≤ (n + m).log2 := by
+  simp only [le_add_iff_nonneg_right, zero_le, log2_mono]
+
+@[grind ., grind! .]
+lemma log2_le_log2_succ (n : ℕ) : n.log2 ≤ (n + 1).log2 := by
+  simp only [le_add_iff_nonneg_right, zero_le, log2_mono]
