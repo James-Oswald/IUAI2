b0b9c97725ec94142aa118c2becb7443ca0dd2bd
James Oswald <jamesoswald111@gmail.com>
2026-02-25 00:18:58 +0000

    more tm stuff

--- IUAI2/c26/TuringMachine.lean
+++ IUAI2/c26/TuringMachine.lean
@@ -18,7 +18,41 @@ class TM (_ : Type) where
   F : Set Q
 
 -- The type of tapes for a TM
-abbrev TM.Tape (M: Type) [TM M] : Type := Stream' M.Γ
+abbrev TM.Tape (α : Type) [TM α] : Type := Stream' (TM.Γ α)
+
+-- def TM.first_blank (α : Type) [TM α] : Nat :=
+
+-- def TM.string (α : Type) [TM α] TM α
+
+-- The full state of a TM at a single timestep
+structure TM.Config (α : Type) [TM α] where
+  state: TM.Q α
+  tape: TM.Tape α
+  head : Nat
+
+--Single step the TM
+def TM.step {α : Type} [TM α] (C: TM.Config α): Option (TM.Config α) :=
+  -- Read the symbol at the head
+  let symbol : TM.Γ α := C.tape C.head
+  let next : Option (TM.Q α × TM.Γ α × Fin 2) := (TM.δ ⟨C.state, symbol⟩)
+  if H : next.isSome then
+    let ⟨nState, nSymb, dir⟩ := next.get H
+    let nTape := (C.tape.take C.head) ++ₛ ((C.tape.drop C.head).cons nSymb)
+    let nHead := match dir with | 0 => C.head.pred | 1 => C.head.succ
+    some ⟨nState, nTape, nHead⟩
+  else
+    none
+
+-- Run the TM until it halts, if ever
+noncomputable def TM.run {α : Type} [TM α] (x : TM.Tape α) : TM.Tape α :=
+  let init_cfg : TM.Config α := ⟨TM.q0 α, x, 0⟩
+  let rec loop (C : TM.Config α) : TM.Config α :=
+    let ns := TM.step C
+    if ns.isNone
+
+
+
+
 
 inductive Symb where
 | bit : Bool -> Symb
@@ -29,26 +63,3 @@ structure TMC where
   Q_fin : Fintype Q
   δ : Q × Symb -> Option (Q × Symb × Fin 2)
   F : Set Q
-
-
--- The full state of a TM at a single timestep
-structure TM.Config (M: TM) where
-  state: M.Q
-  tape: M.Tape
-  head : Nat
-
---Single step the TM
-def TM.step (M : TM) (C: M.Config): Option M.Config :=
-  -- Read the symbol at the head
-  let symbol : M.Γ := C.tape C.head
-  let next : Option (M.Q × M.Γ × Fin 2) := (M.δ ⟨C.state, symbol⟩)
-  if H : next.isSome then
-    let ⟨nState, nSymb, dir⟩ := next.get H
-    let nTape := (C.tape.take C.head) ++ₛ ((C.tape.drop C.head).cons nSymb)
-    let nHead := match dir with | 0 => C.head.pred | 1 => C.head.succ
-    some ⟨nState, nTape, nHead⟩
-  else
-    none
-
--- Run the TM until it halts, if ever
-noncomputable def TM.run (M: TM) (x : List M.Γ) : List M.Γ :=
